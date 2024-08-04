bc79efda81bc5e1ce14bd30c4987be0b15318006
Toni Kukurin <toni.kukurin@quantco.com>
2024-08-04 16:02:34 +0200

    ngram-atop-tokenizer

--- notebooks/2408_ngrams.py
+++ notebooks/2408_ngrams.py
@@ -9,7 +9,27 @@ words = tk.memo(tk.utils.data.words)('en')
 # %%
 from tk.models import ngram
 with tk.timed():
-    tokenizer = ngram.Tokenizer.init(shakespeare, mincount=2, maxn=16)
+    tokenizer = ngram.Tokenizer.init(shakespeare, mincount=2, maxn_or_tuples=(1, 2, 4, 6, 8, 10, 12, 14, 16, 32))
 # %%
 print(ngram.generate(tokenizer, "hello!"))
+# %%
+from transformers import AutoTokenizer
+tokenizer_gpt2 = AutoTokenizer.from_pretrained("gpt2")
+# %%
+shakespeare_tokens = tokenizer_gpt2.tokenize(shakespeare)
+print(shakespeare_tokens[:10])
+# %%
+with tk.timed('ngram_tokens'):
+    tokenizer_atop_gpt2 = ngram.Tokenizer.init(
+        shakespeare_tokens,
+        mincount=2,
+        maxn_or_tuples=5,
+    )
+# %%
+start = tokenizer_gpt2.tokenize("hello!")
+out, _ = ngram.generate(tokenizer_atop_gpt2, start)
+print(out)
+# %%
+out = tokenizer_gpt2.convert_tokens_to_string(out)
+print(out)
 # %%
--- src/tk/models/ngram.py
+++ src/tk/models/ngram.py
@@ -17,8 +17,7 @@ def to_transition_row(
     for dst, count in dict_entry.items():
         row[stoi[dst]] = count + smooth
     row = np.array(row)
-    sumrow = np.sum(row)
-    if sumrow == 0:
+    if (sumrow := np.sum(row)) == 0:  # uniform
         return np.ones_like(row) / len(row)
     return row / sumrow
 
@@ -36,26 +35,31 @@ class Tokenizer:
         }
         self.transitions = transitions
 
-        self.output_vocab = defaultdict(int)
+        self.vocab = defaultdict(int)
         for dst, count in it.chain(*(x.items() for x in self.transitions.values())):
-            self.output_vocab[dst] += count
+            self.vocab[dst] += count
 
-        self.stoi = {c: i for i, c in enumerate(self.output_vocab)}
+        self.stoi = {c: i for i, c in enumerate(self.vocab)}
         self.itos = {i: c for c, i in self.stoi.items()}
-        assert all(len(k) == 1 for k in self.output_vocab)
-        assert all(len(v) == 1 for v in self.stoi)
+        #assert all(len(k) == 1 for k in self.vocab)
+        #assert all(len(v) == 1 for v in self.stoi)
 
     @classmethod
     def init(
         cls,
         text: str,
         mincount: int = 2,
-        maxn: int = 2,
+        maxn_or_tuples: int | tuple = 2,
     ) -> Tokenizer:
         ctr = Counter()
-        for n in range(1, maxn + 1):
+        ns = (
+            maxn_or_tuples 
+            if hasattr(maxn_or_tuples, "__len__")
+            else range(1, maxn_or_tuples + 1)
+        )
+        for n in ns:
             idxs = range(0, len(text) - n)
-            text_chunks = [text[i:i+n] for i in idxs]
+            text_chunks = [tuple(text[i:i+n]) for i in idxs]
             continuations = (nxt for *_, nxt in text_chunks[1:])
             ctr_cur = Counter(zip(text_chunks, continuations))
             ctr += ctr_cur
@@ -66,13 +70,18 @@ class Tokenizer:
         return cls(pruned_counts)
 
 
-def generate(self: Tokenizer, start: str | None = None, max_tokens: int = 64, smooth = 0):
+def generate(
+    self: Tokenizer, 
+    start: str | None = None, 
+    max_tokens: int = 64, 
+    smooth = 0
+):
     def _random() -> str:
         row = to_transition_row(
-            self.output_vocab,
+            self.vocab,
             smooth=smooth,
             stoi=self.stoi,
-            vocab_size=len(self.output_vocab)
+            vocab_size=len(self.vocab)
         )
         choice = np.random.multinomial(1, row).argmax(0)
         return self.itos[choice]
@@ -81,16 +90,18 @@ def generate(self: Tokenizer, start: str | None = None, max_tokens: int = 64, sm
     maxn = max(map(len, self.transitions))
     for _ in range(max_tokens):
         lookback = min(len(outputs), maxn) + 1
-        lasts = (''.join(outputs[-i:]) for i in range(1, lookback))
+        lasts = (tuple(outputs[-i:]) for i in range(1, lookback))
         lasts_with_transitions = [l for l in lasts if l in self.transitions]
-        src = np.random.choice(lasts_with_transitions)
+        src = np.random.choice(len(lasts_with_transitions))
+        src = lasts_with_transitions[src]
         row = to_transition_row(
             self.transitions[src],
             smooth=smooth,
             stoi=self.stoi,
-            vocab_size=len(self.output_vocab)
+            vocab_size=len(self.vocab)
         )
         choice = np.random.multinomial(1, row).argmax(0)
         outputs.append(self.itos[choice])
 
-    return ''.join(outputs), {}
+    return outputs, {}
+    # return ''.join(outputs), {}
