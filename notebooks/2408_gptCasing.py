aede14aba75bc4aded21275e61552453e93eefc3
Toni Kukurin <toni.kukurin@quantco.com>
2024-08-29 21:24:30 +0200

    add some thoughts and better reprs

--- notebooks/2408_gptCasing.py
+++ notebooks/2408_gptCasing.py
@@ -8,6 +8,8 @@ Also see [howard].
 """
 # %%
 from collections import defaultdict
+
+import tqdm
 import tk
 import openai
 from openai.types.chat.chat_completion import ChatCompletion
@@ -18,67 +20,125 @@ from tk.utils import memo
 
 
 with Path("~/.apikeys.json").expanduser().open() as f:
-    key = json.load(f)['openai-self']
-    client = openai.Client(api_key=key)
+  key = json.load(f)['openai-self']
+  client = openai.Client(api_key=key)
 seed = 42
 query = memo(client.chat.completions.create)
 # %%
 import itertools as it
+import numpy as np
+
+from dataclasses import dataclass
+from typing import Callable, NamedTuple
+from jax import tree
+
+
+class In(NamedTuple):
+  variant: str
+  query: str
+  gt: str
+  prompt = property(
+      lambda s: f"answer with yes/no in {s.variant}: {s.query}")
+  __str__ = prompt.fget
+
+
+class Out(NamedTuple):
+  input: In
+  response: ChatCompletion
+  text = property(lambda s: s.response.choices[0].message.content)
+  __str__ = text.fget
+  __repr__ = lambda s: f"Out({s.input}|{s.text})"
 
 variants = [
     "all lowercase",
     "all uppercase",
     "all camelcase",
 ]
-questions = [
-    "are there black swans?",
-    "are there white swans?",
-    "are there gray swans?",
-    "is brunch before lunch?",
-    "is lunch before brunch?",
-]
-responses: list[ChatCompletion] = []
-for question, variant in it.product(questions, variants):
-    response = query(
-      model="gpt-4o-mini",
-      messages=[
-            {"role": "system", "content": "you are a helpful assistant."},
-            {"role": "user", "content": f"answer with yes/no in {variant}: {question}"}
-        ],
-      temperature=1,
-      seed=seed,
-      logprobs=True,
-      top_logprobs=5,
-    )
-    responses.append(response)
+questions = {
+    "y": [
+        "are there black swans?",
+        "are there white swans?",
+        "is brunch before lunch?",
+        "is brunch after breakfast?",
+    ],
+    "?": [
+        "are there gray swans?",
+        "is blue better than yellow?",
+        "are flortls more lucrative than wollys?",
+    ],
+    "n": [
+        "is lunch before brunch?",
+        "is the tallest building in the world over 1 lightyear high?",
+        "is the smallest building in the world over 1km high?",
+        "is the smallest building in the world over 1km high?",
+    ]
+}
+kvs = ((q, gt) for gt, qs in questions.items() for q in qs)
+responses: list[Out] = []
+for (q, gt), variant in tqdm.tqdm(it.product(kvs, variants)):
+  input_ = In(variant=variant, query=q, gt=gt)
+  response = query(
+    model="gpt-4o-mini",
+    messages=[
+          {"role": "system", "content": "you are a helpful assistant."},
+          {"role": "user", "content": input_.prompt}
+      ],
+    temperature=1,
+    seed=seed,
+    logprobs=True,
+    top_logprobs=5,
+  )
+  responses.append(Out(input_, response))
 
+# %%
+print(type(responses[-1]))
 # %%
 str2r = defaultdict(list)
 for r in responses:
-    k = r.choices[0].message.content.lower().strip(' .,!?')
-    str2r[k].append(r)
+  k = r.response.choices[0].message.content.lower().strip(' .,!?')
+  str2r[k].append(r)
 print({k: len(v) for k, v in str2r.items()})
 # %%
-from jax import tree
-import numpy as np
+print(str2r['yes'][0])
+# %%
+class TopProb(NamedTuple):
+  token: str
+  prob: float
+  __str__ = lambda s: s.token
+  __repr__ = lambda s: f"tp({s.token}|{s.prob:.2f})"
 
-def as_toks(r: ChatCompletion, k: int = 2):
-    lps = r.choices[0].logprobs.content[0].top_logprobs[:k]
-    return [(lp.token, np.exp(lp.logprob)) for lp in lps]
+class OutTok(NamedTuple):
+  ref: Out
+  tps: list[TopProb]
 
-def fmt(tpl: tuple):
-    t, p = tpl
-    return (t, int(p * 10000) / 100)
+def as_toks(cc: ChatCompletion, k: int = 2):
+  toks = cc.choices[0].logprobs.content[0].top_logprobs[:k]
+  return [TopProb(t.token, np.exp(t.logprob)) for t in toks]
+
+def as_tps(o: Out) -> OutTok:
+  return OutTok(o, as_toks(o.response))
 
 # just manually inspect
-leafn = lambda x: isinstance(x, tuple)
-xs = tree.map(as_toks, str2r)
-xs = tree.map(fmt, xs, is_leaf=leafn)
+ii = lambda *ty: lambda x: isinstance(x, ty)
+xs = tree.map(as_tps, str2r, is_leaf=ii(Out))
 # %%
-vals, defn = tree.flatten(xs, is_leaf=lambda x: isinstance(x, list) and leafn(x[0]))
+vals, defn = tree.flatten(xs, is_leaf=ii(OutTok))
 # %%
 print(vals)
 # interestingly, variants of the following emerge:
-# 1. ("yes", " yes", ...)
+# 1. ("yes", " yes", ...)  # note the space in 2nd resp
 # 2. ("yes", "no", ...)
 # %%
+from rich import print as rprint
+vals: list[OutTok]
+uncertain = [
+    x
+    for x in vals
+    if str(x.tps[0].token).strip().lower()
+    != str(x.tps[1].token).strip().lower()
+]
+rprint("Uncertain, as defined by yes/no two top-answers:")
+rprint(uncertain)
+# %% analysis says:
+# idk, seems the ambiguous questions kinda depend on casing
+# TODO: construct more ambiguous questions => test
