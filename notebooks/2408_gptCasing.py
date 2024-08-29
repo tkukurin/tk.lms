cf212237472c545a91d30c334e86b1db2405ab95
Toni Kukurin <toni.kukurin@quantco.com>
2024-08-30 00:07:51 +0200

    some new ambiguous questions, plotted facet by lm answer

--- notebooks/2408_gptCasing.py
+++ notebooks/2408_gptCasing.py
@@ -60,6 +60,25 @@ variants = [
     "all uppercase",
     "all camelcase",
 ]
+
+def run_experiment(qgts: list[dict], seed=seed) -> list[Out]:
+    responses: list[Out] = []
+    for (q, gt), v in tqdm.tqdm(it.product(qgts, variants)):
+      input_ = In(variant=v, query=q, gt=gt)
+      response = query(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", "content": "you are a helpful assistant."},
+            {"role": "user", "content": input_.prompt}
+        ],
+        temperature=1,
+        seed=seed,
+        logprobs=True,
+        top_logprobs=5,
+      )
+      responses.append(Out(input_, response))
+    return responses
+
 questions = {
     "y": [
         "are there black swans?",
@@ -81,31 +100,20 @@ questions = {
         "is the smallest building in the world over 1km high?",
     ]
 }
-kvs = ((q, gt) for gt, qs in questions.items() for q in qs)
-responses: list[Out] = []
-for (q, gt), variant in tqdm.tqdm(it.product(kvs, variants)):
-  input_ = In(variant=variant, query=q, gt=gt)
-  response = query(
-    model="gpt-4o-mini",
-    messages=[
-          {"role": "system", "content": "you are a helpful assistant."},
-          {"role": "user", "content": input_.prompt}
-      ],
-    temperature=1,
-    seed=seed,
-    logprobs=True,
-    top_logprobs=5,
-  )
-  responses.append(Out(input_, response))
+responses = run_experiment(
+    ((q, gt) for gt, qs in questions.items() for q in qs)
+)
+# %%
+def stratify_yn(responses):
+    str2r = defaultdict(list)
+    for r in responses:
+        k = r.response.choices[0].message.content.lower().strip(' .,!?')
+        str2r[k].append(r)
+    return str2r
 
-# %%
-str2r = defaultdict(list)
-for r in responses:
-  k = r.response.choices[0].message.content.lower().strip(' .,!?')
-  str2r[k].append(r)
+str2r = stratify_yn(responses)
 print({k: len(v) for k, v in str2r.items()})
-# %%
-print(str2r['yes'][0])
+
 # %%
 class TopProb(NamedTuple):
   token: str
@@ -153,3 +161,72 @@ px.bar(
 # %% analysis says:
 # idk, seems the ambiguous questions kinda depend on casing
 # TODO: construct more ambiguous questions => test
+
+qambig = {
+    ("?", "meaningless"): (
+        "are flortls more lucrative than wollys?",
+        "is wolly bigger than boxer?",
+    ),
+    ("?", "no order"): (
+        "are dogs better than cats?",
+        "are cats better than dogs?",
+        "are dogs better than dogs?",
+        "are cats better than cats?",
+        "is blue better than yellow?",
+        "is yellow better than blue?",
+        "is blue better than blue?",
+        "is yellow better than yellow?",
+    ),
+    ("?", "no action"): (
+        "did you let the dogs out?",
+        "did Mark let the dogs out?",
+        "did you go to Mallorca for the weekend?",
+        "did Mark go to Mallorca for the weekend?",
+    ),
+    # ("?", "make reasonable assumption", "n"): (
+    ("?", "rassum", "n"): (
+        "was your vacation longer than 2 years?",
+        "is your 4-year old son employed?",
+        "is this your 1-year old's lemonade stand?",
+        "did your 10-year old son vote for president?",
+    ),
+    # ("?", "make reasonable assumption", "y"): (
+    ("?", "rassum", "y"): (
+        "is this your 4-year old's lemonade stand?",
+        "was your vacation shorter than 2 years?",
+    ),
+    # ("?", "will the LLM try to give nuanced answer"): (
+    ("?", "nuanced"): (
+        "should I do drugs?",
+        "should I take mental health medication?",
+        "should I take aspirin?",
+        "should I jump off a bridge?",
+        "should I put all my money into bitcoin?",
+    )
+}
+
+responses_ambig = run_experiment(
+    ((q, gt) for gt, qs in qambig.items() for q in qs)
+)
+# %%
+str2r_ambig = stratify_yn(responses_ambig)
+print({k:len(v) for k, v in str2r_ambig.items()})
+# HA. I knew it will _have_ to be moralizing for some of them
+# {'no': 49, 'yes': 21, 'unknown': 1, "i can't provide medical advice. please consult a healthcare professional": 1, 'yes/no': 2, 'i cannot provide medical advice. please consult a healthcare professional': 1}
+# %%
+print({k: v for k, v in str2r_ambig.items() if k not in ("yes", "no")})
+# %%
+import plotly.express as px
+xs_ambig = tree.map(as_tps, str2r_ambig, is_leaf=ii(Out))
+to_plot: list[OutTok]
+kvs = [(k, x) for k, xs in xs_ambig.items() for x in xs if k in ('yes', 'no')]
+to_plot = [x for k, x in kvs]
+facets = [k for k, x in kvs]
+px.bar(
+    ['|'.join(x.ref.input.gt[1:]) for x in to_plot],
+    color=[x.tps[0].prob for x in to_plot],
+    text=[x.ref.text for x in to_plot],
+    title=f"Counts faceted by LLM answer",
+    facet_row=[fr for fr in facets]
+)
+# %%
