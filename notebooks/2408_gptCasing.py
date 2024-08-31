a35cf121f6b681822858ac8fb43b47b769c5f3e6
Toni Kukurin <toni.kukurin@quantco.com>
2024-08-31 13:56:47 +0200

    add some more examples and check entropy on top2, kinda seems something interesting is there

--- notebooks/2408_gptCasing.py
+++ notebooks/2408_gptCasing.py
@@ -7,6 +7,8 @@ Also see [howard].
 [howard]: https://x.com/jeremyphoward/status/1828880731961209094
 """
 # %%
+from tk.utils import tree as tkt
+from tk.utils import pprint  as tkp
 from collections import defaultdict
 from rich import print as rprint
 
@@ -31,6 +33,20 @@ seed = 42
 query = memo(client.chat.completions.create)
 
 # %%
+import regex as re
+
+
+def completion_by_id(xs: tkt.Tree[ChatCompletion], id: str):
+    def f(x):
+        if not ii(ChatCompletion)(x):
+            return None
+        return x if re.search(x.id, id) else None
+
+    xs = tkt.map(f, xs, is_leaf=ii(ChatCompletion))
+    xs, _ = tkt.flatten(xs)
+    return tuple(filter(bool, xs))
+
+# %%
 import itertools as it
 import numpy as np
 
@@ -38,6 +54,8 @@ from dataclasses import dataclass
 from typing import Callable, NamedTuple
 from jax import tree
 
+# single-character __repr__s
+# 🍉 vs 🍓 => have pits on the inside/out (ha, get it)
 
 class In(NamedTuple):
   variant: str
@@ -46,14 +64,17 @@ class In(NamedTuple):
   prompt = property(
       lambda s: f"answer with yes/no in {s.variant}: {s.query}")
   __str__ = prompt.fget
+  __repr__ = lambda s: f"🍉【{s.variant}•{s.query}】"
 
+print(repr(In("a", "b", "c")))
+# %%
 
 class Out(NamedTuple):
   input: In
   response: ChatCompletion
   text = property(lambda s: s.response.choices[0].message.content)
   __str__ = text.fget
-  __repr__ = lambda s: f"Out({s.input}|{s.text})"
+  __repr__ = lambda s: f"🍓【{s.input!r}•{s.text}】"
 
 variants = [
     "all lowercase",
@@ -112,22 +133,24 @@ def stratify_yn(responses):
     return str2r
 
 str2r = stratify_yn(responses)
-print({k: len(v) for k, v in str2r.items()})
+tkp.p({k: len(v) for k, v in str2r.items()})
 
 # %%
 class TopProb(NamedTuple):
-  token: str
+  tok: str
   prob: float
-  __str__ = lambda s: s.token
-  __repr__ = lambda s: f"tp({s.token}|{s.prob:.2f})"
+  __str__ = lambda s: s.tok
+  __repr__ = lambda s: f"{s.tok}•{s.prob:.2f}"
 
 class OutTok(NamedTuple):
   ref: Out
-  tps: list[TopProb]
+  tps: tuple[TopProb]
+  __repr__ = lambda s: f"【{s.ref!r}•{s.tps!r}】"
+
 
 def as_toks(cc: ChatCompletion, k: int = 2):
   toks = cc.choices[0].logprobs.content[0].top_logprobs[:k]
-  return [TopProb(t.token, np.exp(t.logprob)) for t in toks]
+  return tuple(TopProb(t.token, np.exp(t.logprob)) for t in toks)
 
 def as_tps(o: Out) -> OutTok:
   return OutTok(o, as_toks(o.response))
@@ -137,30 +160,54 @@ ii = lambda *ty: lambda x: isinstance(x, ty)
 xs = tree.map(as_tps, str2r, is_leaf=ii(Out))
 # %%
 vals, defn = tree.flatten(xs, is_leaf=ii(OutTok))
-# %%
-rprint(vals[:3])
+tkp.p(vals[:3])
 # interestingly, variants of the following emerge:
 # 1. ("yes", " yes", ...)  # note the space in 2nd resp
 # 2. ("yes", "no", ...)
 # %%
-vals: list[OutTok]
-uncertain = [
-    x
-    for x in vals
-    if str(x.tps[0].token).strip().lower()
-    != str(x.tps[1].token).strip().lower()
-]
-# %%
 import plotly.express as px
+import plotly.io as pio
+from IPython.display import display_png
+
+
+def semantically_differ(*tps: TopProb):
+    """check if tokens semantically differ.
+
+    our answers should be yes/no so don't overcomplicate.
+    """
+    xs = {t.tok.strip(' .?:!').lower() for t in tps}
+    return len(xs) == len(tps)
+
+
+def metric_uncertainty_top2(*tps: TopProb):
+    if not semantically_differ(*tps):
+        return 0.0
+    probs = np.array([t.prob for t in tps])
+    entropy = np.sum(-np.log(probs) * probs)
+    return entropy
+
+
+uncertain = (x for x in vals if semantically_differ(*x.tps))
 sorteds = sorted(uncertain, key=lambda x: x.tps[0].prob)
-px.bar(
+to_print = []
+for i, u in enumerate(sorteds):
+    to_print.append((
+        u, 
+        metric_uncertainty_top2(*u.tps)))
+tkp.p(to_print)
+# %%
+fig = px.bar(
     [x.ref.input.gt for x in sorteds],
     color=[x.tps[0].prob for x in sorteds],
+    text=[x.tps[0].tok for x in sorteds],
     title="Uncertain (per top-two differing answers)"
 )
+fig.update_traces(textposition='inside', )
+fig_img = pio.to_image(fig)
+display_png(fig_img, raw=True)
 # %% analysis says:
 # idk, seems the ambiguous questions kinda depend on casing
-# TODO: construct more ambiguous questions => test
+# below: construct more ambiguous questions => test
 
 qambig = {
     ("?", "meaningless"): (
@@ -222,11 +269,12 @@ to_plot: list[OutTok]
 kvs = [(k, x) for k, xs in xs_ambig.items() for x in xs if k in ('yes', 'no')]
 to_plot = [x for k, x in kvs]
 facets = [k for k, x in kvs]
-px.bar(
+fig = px.bar(
     ['|'.join(x.ref.input.gt[1:]) for x in to_plot],
     color=[x.tps[0].prob for x in to_plot],
     text=[x.ref.text for x in to_plot],
     title=f"Counts faceted by LLM answer",
-    facet_row=[fr for fr in facets]
+    facet_row=[fr for fr in facets],
 )
+fig.update_traces(textposition='inside', )
 # %%
