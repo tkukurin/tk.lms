856de11a1ee343e22016962c61ba2f72836a6df4
Toni Kukurin <toni.kukurin@quantco.com>
2024-10-19 21:44:27 +0200

    warn on wrong install

--- src/tk/utils/utils.py
+++ src/tk/utils/utils.py
@@ -4,7 +4,6 @@ import itertools as it
 
 import requests
 import diskcache
-from loguru import logger
 from pathlib import Path
 
 from typing import Any, Callable
@@ -12,29 +11,47 @@ from types import SimpleNamespace as nspc
 from time import time
 from contextlib import contextmanager
 
+from loguru import logger as L
+from torch.nn.functional import max_pool1d_with_indices
+# TODO rich
+# from rich import logging
 
 rootdir = Path(__file__).parent.parent.parent.parent
+# will be sth like '/.../.venv/lib/python3.11/...'
+if '/lib/python' in str(rootdir.absolute()):
+    L.warning(
+        f"I think we are running in venv: {rootdir}."
+        "Suggest to install via `pip install -e .`"
+    )
 datadir = rootdir / 'data'
 cache = diskcache.Cache(datadir / "cache")
 
 
+def shrt(text: str, to: int = 10) -> str:
+    """Shorten text to `to` characters."""
+    from rich.text import Text
+    t = Text(text)
+    t.truncate(to, overflow="ellipsis")
+    return str(t)
+
+
 def memo(f: Callable, **kw):
     """Use instead of manual caching.
-    
-    E.g. 
+
+    E.g.
         >>> fetch(url, cache_file)
         >>> data = open(cache_file)
         >>> # instead ...
         >>> data = utils.memo(fetch)(url)
-    
+
     """
     return cache.memoize(**kw)(f)
 
 
-def fetch(url: str) -> bytes:
-    logger.debug(f"Downloading: {url}")
+def fetch(url: str) -> bytes | None:
+    L.debug(f"Downloading: {url}")
     if not (got := requests.get(url)).ok:
-        logger.warning(f"some error downloading: {got}")
+        L.warning(f"some error downloading: {got}")
         return None
     else:
         data = got.content
@@ -42,7 +59,7 @@ def fetch(url: str) -> bytes:
 
 
 @contextmanager
-def timed(name: Any = "_", show: Callable = logger.debug):
+def timed(name: Any = "_", show: Callable = L.debug):
     """Use to ad-hoc time.
     """
     val = nspc(name=name, t1=None, t2=None)
