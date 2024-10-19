3400aa8b96820bfecda3df4579d0aa075b7d905c
Toni Kukurin <toni.kukurin@quantco.com>
2024-10-19 16:27:04 +0200

    logging via aim, prefer char tokenizer, get_batch

--- notebooks/2410_trainGptTransformers.py
+++ notebooks/2410_trainGptTransformers.py
@@ -5,36 +5,130 @@ import jax
 import jax.numpy as jnp
 import optax
 from transformers import FlaxGPT2LMHeadModel, GPT2Tokenizer, GPT2Config
+from transformers.models.gpt2.modeling_flax_gpt2 import FlaxGPT2Model
 
-tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
-tokenizer.pad_token = tokenizer.eos_token
+# tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+# tokenizer.pad_token = tokenizer.eos_token
 data = [
     "def my_function(overfit=True):\n    \"hello here I am overfitting as hell\"\n    return 1",
     "def my_other_function(overfit=False):\n    return 0"
 ]
-inputs = tokenizer(data, return_tensors="jax", padding=True, truncation=True)
+# %%
+from tk.utils.data import tokenizer as toklib
+import importlib
+import string
+importlib.reload(toklib)
+cfg, tokenizer = toklib.mktokenizer(
+    string.printable + string.whitespace,
+    max_len=64,
+    return_og_tokenizer=False,
+)
+print(tokenizer.vocab_size, len(tokenizer.get_vocab()))
+# %%
+print(tokenizer("def test").input_ids)
+print(tokenizer("def test def test def test", max_length=6, return_overflowing_tokens=True).input_ids)
+print(tokenizer("def test<|eof|>def test def test", max_length=6, return_overflowing_tokens=True).input_ids)
+print("EOS:", tokenizer(tokenizer.eos_token, return_overflowing_tokens=True).input_ids)
+# TODO figure out why this breaks :(
+# inuition/hypothesis: pre-tokenizer should be fixed.
+# use some regex to prefer recog of special tokens
+print("EOS:", tokenizer(f"as{tokenizer.eos_token}df{tokenizer.eos_token}asdfasdf", return_overflowing_tokens=True).input_ids)
+# %%
+print(tokenizer("def test", add_special_tokens=False).input_ids)
+print(tokenizer.decode(tokenizer("def test", add_special_tokens=False).input_ids))
+print(tokenizer.decode(tokenizer("def test").input_ids, skip_special_tokens=False))
+print(tokenizer.decode(tokenizer("def test").input_ids, skip_special_tokens=True))
+print(type(tokenizer))
+# %%
+inputs = tokenizer(
+    tokenizer.eos_token.join(data),
+    return_tensors="jax",
+    padding=False,#"max_length",
+    truncation=False,
+    max_length=None,
+    # stride=16,
+    # return_overflowing_tokens=True,
+    # add_special_tokens=False,
+)
 
-# model = FlaxGPT2LMHeadModel.from_pretrained("gpt2")
-model = FlaxGPT2LMHeadModel(GPT2Config(vocab_size=50257, n_positions=1024))
+print(inputs.input_ids.shape)
+for i in inputs.input_ids[:3]:
+    print()
+    print(tokenizer.convert_ids_to_tokens(i[:10]))
+    print(tokenizer.convert_ids_to_tokens(i[-10:]))
+# %%
+print(inputs.input_ids.max())
+# NB, source of nasty bugs!!
+print(len(tokenizer.get_vocab()), tokenizer.vocab_size)
+# %%
+def get_batch(data, rng, batch_size=8, block_size=tokenizer.model_max_length):
+    # NB, in theory when dealing with small data this is actually really important
+    # i.e. we will never sample (len(data) - block_size) to len(data)
+    ix = jax.random.randint(
+        rng, shape=(batch_size,), minval=0, maxval=len(data) - block_size)
+    x = jnp.stack([data[i:i+block_size] for i in ix])
+    # we handle this in train_step
+    # y = jnp.stack([data[i+1:i+1+block_size] for i in ix])
+    return x
+
+
+print(inputs.input_ids.shape)
+print("get_batch:", get_batch(inputs.input_ids.flatten(), rng).shape)
+# %%
+from transformers.models.gpt2 import FlaxGPT2LMHeadModel
+model = FlaxGPT2LMHeadModel(
+    GPT2Config(
+        vocab_size=len(tokenizer.get_vocab()),
+        n_positions=tokenizer.model_max_length,
+        pad_token_id=tokenizer.pad_token_id,
+        bos_token_id=tokenizer.bos_token_id,
+        eos_token_id=tokenizer.eos_token_id
+))
+print(tokenizer.model_max_length,)
+rng = jax.random.PRNGKey(0)
+input_shape = (1, tokenizer.model_max_length)
+model.params = model.init_weights(rng, input_shape)
+# %%
+print(inputs.input_ids.shape)
+outs = model(
+    input_ids=get_batch(inputs.input_ids.flatten(), rng),
+    attention_mask=None,
+    return_dict=True,
+    params=model.params,
+    dropout_rng=rng,
+    train=True,
+)
+print(outs.logits.shape)
+assert not jnp.isnan(outs.logits.sum()), "BUG: NaN in logits"
+# %%
 optimizer = optax.adam(1e-4)
 opt_state = optimizer.init(model.params)
 
 
-def train_step(params, opt_state, input_ids, attention_mask):
+@jax.jit
+def train_step(params, opt_state, input_ids, rng):
     def loss_fn(params):
-        logits = model(
-            input_ids=input_ids, attention_mask=attention_mask, params=params
-        ).logits
-
-        loss =  optax.softmax_cross_entropy(
+        outs = model(
+            input_ids=input_ids,
+            # attention_mask=attention_mask,
+            return_dict=True,
+            params=params,
+            dropout_rng=rng,
+            train=True,
+        )
+        logits = outs.logits
+        logits = jnp.clip(logits, -1e9, 1e9)  # Clip logits to a reasonable range
+        loss = optax.softmax_cross_entropy(
             logits[:, :-1],
             jax.nn.one_hot(input_ids[:, 1:], logits.shape[-1])
         )
-        masked_loss = loss * attention_mask[:, 1:]
-        normalized_loss = masked_loss.sum() / attention_mask[:, 1:].sum()
-        return normalized_loss
+        # we expect standard causal attention, no attn mask required
+        #masked_loss = loss * attention_mask[:, 1:]
+        #normalized_loss = masked_loss.sum() / attention_mask[:, 1:].sum()
+        return loss.mean() #normalized_loss
 
     loss, grads = jax.value_and_grad(loss_fn)(params)
+    grads = jax.tree.map(lambda g: jnp.clip(g, -1.0, 1.0), grads)
     updates, opt_state = optimizer.update(grads, opt_state)
     return optax.apply_updates(params, updates), opt_state, loss
 # %%
@@ -42,7 +136,7 @@ import aim
 import tk
 import tqdm
 
-close_run = True
+close_run = False
 log_dir = tk.datadir / 'logs'
 log_dir.mkdir(parents=True, exist_ok=True)
 
@@ -50,37 +144,89 @@ try:
     if (run_ := locals().get('run')) and run_.active:
         print("Reusing existing run.")
         run = run_
-except:
+except:  # idk run_.active can fail, maybe bug
     run = None
 
 if not (run_ := locals().get('run')):
     print("New run.")
-    run = aim.Run(repo=log_dir)
-    run.read_only
+    run = aim.Run(repo=log_dir, experiment='2410_gpt2code')
 
 print(f"{close_run=}")
+# %%
+L = tk.utils.log.logger
 
-for epoch in tqdm.trange(10):
-    model.params, opt_state, loss = train_step(
-        model.params,
-        opt_state,
-        inputs["input_ids"],
-        inputs["attention_mask"]
+
+def sample_text(prompt: str):
+    inputs = tokenizer(
+        [prompt],
+        return_tensors="jax",
+        padding=True,
+        truncation=True,
+        add_special_tokens=False,
     )
-    print(loss)
-    run.track(loss, name='loss', epoch=epoch)
+    # inputs.pop("token_type_ids", None)
+    output_ids = model.generate(**inputs, max_length=50)
+    output_ids = output_ids.sequences
+    return tokenizer.decode(
+        output_ids[0], skip_special_tokens=True)
+
+
+epoch = locals().get('epoch', 0)
+run = locals().get('run')
+assert isinstance(run, aim.Run), f'stuff went awry logging {run=}'
+# TODO tokenizer should know to handle <EOS> token
+# however it seems to split on < and >, so we need to handle this manually
+input_ids = []
+for d in data:
+    cur_ids = tokenizer(
+        d,
+        return_tensors="jax",
+        max_length=None,
+    ).input_ids
+    input_ids.append(cur_ids)
+    input_ids.append(jnp.array([[tokenizer.eos_token_id]]))
+input_ids = jnp.hstack(input_ids).flatten()
+L.info(f"Inputs: {input_ids.shape}")
+# not needed since model is handling this
+# train_step_batched = jax.vmap(train_step, in_axes=(None, None, 0, 0))
+rng = jax.random.PRNGKey(42)
+with tqdm.trange(epoch, epoch + 20, desc="Epoch") as t:
+    for epoch in t:
+        rng, batch_rng = jax.random.split(rng)
+        x = get_batch(
+            input_ids,
+            rng=batch_rng,
+            batch_size=8
+        )
+        rng, train_rng = jax.random.split(rng)
+        model.params, opt_state, loss = train_step(
+            model.params,
+            opt_state,
+            x,
+            train_rng,
+        )
+        run.track(loss, name='loss', epoch=epoch)
+        if (epoch + 1) % 10 == 0:
+            t.set_postfix(status="sampling", loss=loss)
+            for prompt in ("def", "x ="):
+                outs = sample_text(prompt)
+                run.track(aim.Text(outs), name='sample', epoch=epoch, context={
+                    'prompt': prompt
+                })
+
+        t.set_postfix(loss=loss)
 
 if close_run:
+    print("Closing run.")
     run.close()
 # %%
-def sample_text(prompt="def"):
-    inputs = tokenizer([prompt], return_tensors="jax", padding=True, truncation=True)
-    output_ids = model.generate(inputs["input_ids"], max_length=50)
-    output_ids = output_ids.sequences
-    return tokenizer.decode(output_ids[0], skip_special_tokens=True)
-
-
 text = sample_text("def")
 print("Done.")
 print("=====")
 print(text)
+
+# %%
+ins = tokenizer("def", return_tensors="jax", padding=True, truncation=True, add_special_tokens=False)
+outs = model.generate(**ins)
+# %%
+tokenizer.batch_decode(outs.sequences)
--- src/tk/utils/data/tokenizer.py
+++ src/tk/utils/data/tokenizer.py
@@ -13,8 +13,8 @@ import tokenizers.implementations as toklib
 import transformers
 from huggingface_hub import unlike
 from loguru import logger
-from tokenizers import Tokenizer, models, pre_tokenizers
-from tokenizers.implementations import CharBPETokenizer
+from tokenizers import Tokenizer, models, pre_tokenizers, AddedToken
+from tokenizers.implementations import CharBPETokenizer, BaseTokenizer
 from tokenizers.processors import TemplateProcessing
 from transformers.tokenization_utils import AddedToken, PreTrainedTokenizer
 
@@ -25,7 +25,7 @@ special_tokens = {
     'bos_token': '<|bof|>',
     'unk_token': '<|unk|>',
     'cls_token': '<|bof|>',  # or cls?
-    'reserved_token': '<|reserved>',
+    'reserved_token': '<|reserved|>',
     'mask_token': '<|mask|>',
 }
 CFG_DEFAULT = transformers.GPT2Config()
@@ -41,40 +41,46 @@ def mkvocab(texts: Iterable, special_tokens: set = set(special_tokens.values()))
 
 
 def mktokenizer_base(data, special_tokens: dict = special_tokens) -> toklib.BaseTokenizer:
-    tokenizer = CharBPETokenizer(suffix="", unk_token=special_tokens['unk_token'])
-    # TODO https://discuss.huggingface.co/t/add-bos-and-eos-when-encoding-a-sentence/21833
-    # ??
+    # tokenizer = CharBPETokenizer(suffix="", unk_token=special_tokens['unk_token'])
+    # https://discuss.huggingface.co/t/add-bos-and-eos-when-encoding-a-sentence/21833
     # https://github.com/huggingface/tokenizers/issues/704
-    # tokenizer = Tokenizer(models.WordLevel(unk_token=special_tokens['unk_token']))
-    # tokenizer.pre_tokenizer = pre_tokenizers.Split("", "isolated")
-
-    sep = special_tokens['sep_token']
-    bos = special_tokens['bos_token']
+    unk = special_tokens['unk_token']
     eos = special_tokens['eos_token']
+
+    tokenizer = Tokenizer(models.WordLevel(
+        vocab=None,
+        unk_token=unk,
+    ))
+    tokenizer.pre_tokenizer = pre_tokenizers.Split(
+        "", "isolated")
+    trainer = tokenizer.model.get_trainer()
+
+    tokenizer.train_from_iterator(
+        data, trainer=trainer
+        # vocab_size=len(vocab_full),
+        # special_tokens=list(set(special_tokens.values())),
+        # min_frequency=1,
+        # initial_alphabet=list(special_tokens),
+        # suffix="",
+    )
+    tokenizer.add_special_tokens(special_tokens := [  # type: ignore
+        AddedToken(eos, special=True, single_word=True),
+        AddedToken(unk, special=True, single_word=True),
+        # AddedToken(bos, special=True, single_word=True),
+        # AddedToken(sep, special=True, single_word=True),
+    ])
+    special_tokens = [
+        (k.content, tokenizer.get_vocab()[k.content])
+        for k in special_tokens
+    ]
     tokenizer.post_processor = TemplateProcessing(
-        single=f"{bos} $A {eos}",
+        single="$A",
         # see docs for TemplateProcessing for deets.
         #  [CLS]   ...   [SEP]   ...   [EOF]
         #    0      0      0      1      1
         pair=None, # f"{bos} $A {sep} $B:1 {eos}:1",
-        special_tokens=[
-            (special_tokens['bos_token'], 0),
-            (special_tokens['eos_token'], 1),
-            (special_tokens['unk_token'], 2),
-        ],
+        special_tokens=special_tokens,
     )
-
-    tokenizer.train_from_iterator(
-        data,
-        # vocab_size=len(vocab_full),
-        special_tokens=list(set(special_tokens.values())),
-        min_frequency=1,
-        # initial_alphabet=list(special_tokens),
-        suffix="",
-    )
-
-    tokenizer.enable_padding(pad_id=1, pad_token=special_tokens['pad_token'])
-
     return tokenizer
 
 
@@ -89,11 +95,11 @@ def tokenizers_to_auto_tokenizer(
         tokenizer = transformers.AutoTokenizer.from_pretrained(
             f"{tmpdir}",
             model_max_length=max_len,
-            vocab_size=tokenizer.get_vocab_size(),
             **special_tokens
         )
         config = transformers.AutoConfig.from_pretrained(f"{tmpdir}")
-    config.vocab_size = tokenizer.vocab_size
+    # NB, idk tokenizer.vocab_size will still be wrong...
+    config.vocab_size = len(tokenizer.get_vocab())
     config.bos_token_id = tokenizer.bos_token_id
     config.eos_token_id = tokenizer.eos_token_id
     config.unk_token_id = tokenizer.unk_token_id
@@ -106,16 +112,27 @@ def tokenizers_to_auto_tokenizer(
             getattr(tokenizer, x),
             getattr(tokenizer, x.replace("_id", ""))
         ])))
+
+    def _decode(x, skip_special_tokens=False, *a, **kw):
+        """idk this is a hacky way to fix the default decode which adds spaces."""
+        id2t = tokenizer.convert_ids_to_tokens(
+            x, skip_special_tokens=skip_special_tokens)
+        return "".join(id2t)
+
+    tokenizer.decode = _decode
     return config, tokenizer
 
 
 def mktokenizer(
-    data=string.printable,
+    data=string.printable + string.whitespace,
     config=CFG_DEFAULT,
     max_len=512,
-    special_tokens: dict = special_tokens
+    special_tokens: dict = special_tokens,
+    return_og_tokenizer: bool = False,
 ):
-    tokenizer = mktokenizer_base(data, special_tokens)
+    tokenizer_og = mktokenizer_base(data, special_tokens)
     config, tokenizer = tokenizers_to_auto_tokenizer(
-        tokenizer, config=config, max_len=max_len)
+        tokenizer_og, config=config, max_len=max_len)
+    if return_og_tokenizer:
+        return config, tokenizer_og, tokenizer
     return config, tokenizer
