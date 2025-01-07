61a418b2e8b3a128e86f60c7f4e45a228bb299ac
Toni Kukurin <tkukurin@gmail.com>
2025-01-07 19:46:35 +0000

    add arc visualize failures

--- notebooks/2412_arcVisualize.py
+++ notebooks/2412_arcVisualize.py
@@ -53,6 +53,23 @@ def visualize_grid(grid) -> plt.Figure:
             color=colors[grid[i, j]]))
     return fig
 
+def visualize_grid_of_grids(*grids):
+    fig, axs = plt.subplots(1, len(grids))
+    grids = list(map(np.array, grids))
+    for i, grid in enumerate(grids):
+        ax = axs[i]
+        ax.set_aspect('equal')
+        ax.axis('off')
+        ax.set_xticks([])
+        ax.set_yticks([])
+        ax.set_xlim(0, grid.shape[1])
+        ax.set_ylim(0, grid.shape[0])
+        for i in range(grid.shape[0]):
+            for j in range(grid.shape[1]):
+                ax.add_patch(Rectangle((j, grid.shape[0] - i - 1), 1, 1, 
+                color=colors[grid[i, j]]))
+    return fig
+
 # %%
 from tk.arc.p3 import get_data
 data = get_data('evaluation')
@@ -60,3 +77,36 @@ data = get_data('evaluation')
 with plt.ioff():
     fig = visualize_grid(data['test']['da515329'][0]['input'])
 fig
+# %%
+
+import json
+import tk
+path = tk.datadir / "ARC-llm_baselines"
+assert path.exists(), "try to git clone, see tk.arc"
+# traverse all json in path / "results"
+dfs = {}
+results = list(path.glob("**/results/**/*.json"))
+print('found', len(results))
+for p in results:
+    with open(p, 'r') as f:
+        dfs[f"{p.parent.name}/{p.stem}"] = json.load(f)
+print("loaded", len(dfs))
+print(dfs.keys())
+# %%
+def get_fails(result: dict):
+    rs = result['task_results']
+    return [r for r, v in rs.items() if v < 1]
+
+print(get_fails(dfs['open_ai_o1_med_20241217/results']))
+# %%
+k = 'open_ai_o1_med_20241217/results' 
+fails = get_fails(dfs[k])
+print(k)
+with plt.ioff():
+    fig = visualize_grid(data['train'][fails[0]][1]['input'])
+    fig = visualize_grid_of_grids(
+        data['train'][fails[0]][0]['input'],
+        data['train'][fails[0]][1]['input'],
+    )
+fig
+# %%
