ed0ba2cb6ac2683f858941e683d98c42338cf8b5
Toni Kukurin <tkukurin@gmail.com>
2025-01-10 00:48:09 +0000

    local model debate

--- src/tk/debate/utils.py
+++ src/tk/debate/utils.py
@@ -1,8 +1,26 @@
+from typing import NamedTuple
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
+
 import time
 
 from tk.utils.log import L
 
 import openai.types.chat.chat_completion as oai_types
+
+from ml_collections import ConfigDict
+from types import SimpleNamespace as nspc
+
+def _oai_adapt(text: str):
+    """Unnecessarily nested adapter for relevant parts of ChatCompletion"""
+    # NB config dict has ugly repr
+    # msg = ConfigDict()
+    # msg.content = text
+    # choice = ConfigDict()
+    # choice.message = msg
+    # mlc = ConfigDict()
+    # mlc.choices = [choice]
+    return nspc(choices=[nspc(message=nspc(content=text))])
 
 
 def construct_assistant_message(completion: oai_types.ChatCompletion):
@@ -10,7 +28,56 @@ def construct_assistant_message(completion: oai_types.ChatCompletion):
     return {"role": "assistant", "content": content}
 
 
-def generate_answer(answer_context) -> oai_types.ChatCompletion:
+class LocalModel:
+    """adhoc singleton running a local generative model.
+
+    Use in place of gpt for local model fueled debate:
+        >>> LocalModel()([{'role': 'user', 'content': '...'}])
+    """
+
+    _instance = None
+    _DEFAULT_GEN_KWS = dict(
+        return_dict_in_generate=True,
+        return_legacy_cache=False,
+        max_new_tokens=512,
+    )
+
+    def __new__(cls):
+        L.info("Creating LocalModel")
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+    def __init__(self):
+        if not hasattr(self, 'model'):
+            model_id = "gg-hf/gemma-2b-it"
+            dtype = torch.bfloat16
+            self.tokenizer = AutoTokenizer.from_pretrained(model_id)
+            self.model = AutoModelForCausalLM.from_pretrained(
+                model_id,
+                device_map="cuda",
+                torch_dtype=dtype,
+            )
+            self.model.eval()
+        else:
+            L.info("LocalModel already initialized")
+
+    def __call__(self, answer_context):
+        inp = self.tokenizer.apply_chat_template(
+            answer_context, 
+            tokenize=True, 
+            add_generation_prompt=True,
+            return_dict=True,
+            return_tensors="pt")
+        inp = {k: v.to(self.model.device) for k, v in inp.items()}
+        out = self.model.generate(
+            **inp, 
+            **self._DEFAULT_GEN_KWS)
+        out_str = self.tokenizer.decode(out['sequences'][0])
+        return _oai_adapt(out_str)
+
+
+def generate_answer_gpt(answer_context) -> oai_types.ChatCompletion:
     try:
         from tk.models.gpt import ApiModel
         create = ApiModel()
@@ -21,7 +88,8 @@ def generate_answer(answer_context) -> oai_types.ChatCompletion:
     except Exception as e:
         L.error(f"API error: {e}")
         time.sleep(20)
-        return generate_answer(answer_context)
-
+        return generate_answer_gpt(answer_context)
     return completion
 
+
+generate_answer = LocalModel()
