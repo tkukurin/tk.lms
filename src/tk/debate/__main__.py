9917be0e4924b81a344b6a8d527af42f8df2b72a
Toni Kukurin <tkukurin@gmail.com>
2025-01-11 10:02:46 +0000

    yapf formatting

--- src/tk/debate/__main__.py
+++ src/tk/debate/__main__.py
@@ -15,66 +15,62 @@ import importlib
 from ml_collections import config_flags
 from tk.debate import utils as dutil
 
-
 CFG = config_flags.DEFINE_config_file(
-    "c",
-    str(Path(__file__).parent / "configs/main.py"),
+    "c", str(Path(__file__).parent / "configs/main.py"),
     "Path to the configuration file"
 )
 
 
 def find_module_with_prefix(package, prefix, task):
-    """Find first module with given prefix in package."""
-    for module_info in pkgutil.iter_modules(package.__path__):
-        if module_info.name.startswith(prefix):
-            return importlib.import_module(
-                f'tk.debate.{task}.{module_info.name}'
-            )
-    return None
+  """Find first module with given prefix in package."""
+  for module_info in pkgutil.iter_modules(package.__path__):
+    if module_info.name.startswith(prefix):
+      return importlib.import_module(f'tk.debate.{task}.{module_info.name}')
+  return None
 
 
 def main(argv):
-    cfg = CFG.value
-    L.info(f"{cfg=}")
-    utils.post_mortem_debug()
+  cfg = CFG.value
+  L.info(f"{cfg=}")
+  utils.post_mortem_debug()
 
-    do_gen = True
-    if data := dutil.load(cfg):
-        L.error("found result, not proceeding with generation. delete.")
-        do_gen = False
+  do_gen = True
+  if data := dutil.load(cfg):
+    L.error("found result, not proceeding with generation. delete.")
+    do_gen = False
 
-    # if cfg.task == "plot":
-    #     from tk.debate.math import plot
-    #     return plot.main(cfg, data)
+  # if cfg.task == "plot":
+  #     from tk.debate.math import plot
+  #     return plot.main(cfg, data)
 
-    dutil.generate_answer = dutil.LocalModel(cfg.model)
+  dutil.generate_answer = dutil.LocalModel(cfg.model)
 
-    try:
-        package = importlib.import_module(f'tk.debate.{cfg.task}')
-        L.info(f'looking in {package.__path__}')
+  try:
+    package = importlib.import_module(f'tk.debate.{cfg.task}')
+    L.info(f'looking in {package.__path__}')
 
-        gen_module = find_module_with_prefix(package, 'gen_', cfg.task)
-        eval_module = find_module_with_prefix(package, 'eval_', cfg.task)
+    gen_module = find_module_with_prefix(package, 'gen_', cfg.task)
+    eval_module = find_module_with_prefix(package, 'eval_', cfg.task)
 
-        if not gen_module and not eval_module:
-            L.error(f"No gen_ or eval_ modules found in {package.__path__}")
-            return
+    if not gen_module and not eval_module:
+      L.error(f"No gen_ or eval_ modules found in {package.__path__}")
+      return
 
-        if gen_module and do_gen:
-            L.info("Running generation module")
-            gen_module.main(cfg=cfg)
-        else:
-            L.warning(f"generation: {do_gen=} (or no module found)")
-        
-        if eval_module:
-            L.info("Running evaluation module")
-            eval_module.main(cfg=cfg)
-        else:
-            L.warning("No eval module found")
+    if gen_module and do_gen:
+      L.info("Running generation module")
+      gen_module.main(cfg=cfg)
+    else:
+      L.warning(f"generation: {do_gen=} (or no module found)")
 
-    except ImportError as e:
-        print(f"Error: {e}")
+    if eval_module:
+      L.info("Running evaluation module")
+      eval_module.main(cfg=cfg)
+    else:
+      L.warning("No eval module found")
+
+  except ImportError as e:
+    print(f"Error: {e}")
 
 
 if __name__ == '__main__':
-    app.run(main)
+  app.run(main)
--- src/tk/debate/gsm/gen_gsm.py
+++ src/tk/debate/gsm/gen_gsm.py
@@ -9,56 +9,76 @@ from tk.utils.log import L
 
 
 def construct_message(agents, question, idx):
-    if len(agents) == 0:
-        return {"role": "user", "content": "Can you double check that your answer is correct. Please reiterate your answer, with your final answer a single numerical number, in the form \\boxed{{answer}}."}
+  if len(agents) == 0:
+    return {
+        "role":
+            "user",
+        "content":
+            "Can you double check that your answer is correct. Please reiterate your answer, with your final answer a single numerical number, in the form \\boxed{{answer}}."
+    }
 
-    prefix_string = "These are the solutions to the problem from other agents: "
+  prefix_string = "These are the solutions to the problem from other agents: "
 
-    for agent in agents:
-        agent_response = agent[idx]["content"]
-        response = "\n\n One agent solution: ```{}```".format(agent_response)
+  for agent in agents:
+    agent_response = agent[idx]["content"]
+    response = "\n\n One agent solution: ```{}```".format(agent_response)
 
-        prefix_string = prefix_string + response
+    prefix_string = prefix_string + response
 
-    prefix_string = prefix_string + """\n\n Using the solutions from other agents as additional information, can you provide your answer to the math problem? \n The original math problem is {}. Your final answer should be a single numerical number, in the form \\boxed{{answer}}, at the end of your response.""".format(question)
-    return {"role": "user", "content": prefix_string}
+  prefix_string = prefix_string + """\n\n Using the solutions from other agents as additional information, can you provide your answer to the math problem? \n The original math problem is {}. Your final answer should be a single numerical number, in the form \\boxed{{answer}}, at the end of your response.""".format(
+      question
+  )
+  return {"role": "user", "content": prefix_string}
 
 
 def read_jsonl(path: str):
-    with open(path) as fh:
-        return [json.loads(line) for line in fh.readlines() if line]
+  with open(path) as fh:
+    return [json.loads(line) for line in fh.readlines() if line]
+
 
 def main(cfg, **kw):
-    dbg = cfg.dbg
-    agents = cfg.agents
-    rounds = cfg.rounds
-    random.seed(cfg.seed)
+  dbg = cfg.dbg
+  agents = cfg.agents
+  rounds = cfg.rounds
+  random.seed(cfg.seed)
 
-    generated_description = {}
-    L.info("Loading gsm4k")
-    dftest = datasets.load_dataset("openai/gsm8k", "main")["test"].to_pandas()
-    questions = dftest.to_dict(orient='records')
-    random.shuffle(questions)
-    n = 2 if dbg else 100
+  generated_description = {}
+  L.info("Loading gsm4k")
+  dftest = datasets.load_dataset("openai/gsm8k", "main")["test"].to_pandas()
+  questions = dftest.to_dict(orient='records')
+  random.shuffle(questions)
+  n = 2 if dbg else 100
 
-    for data in tqdm.tqdm(questions[:n], desc='questions'):
-        question = data['question']
-        answer = data['answer']
+  for data in tqdm.tqdm(questions[:n], desc='questions'):
+    question = data['question']
+    answer = data['answer']
 
-        agent_contexts = [[{"role": "user", "content": """Can you solve the following math problem? {} Explain your reasoning. Your final answer should be a single numerical number, in the form \\boxed{{answer}}, at the end of your response. """.format(question)}] for agent in range(agents)]
+    agent_contexts = [
+        [
+            {
+                "role":
+                    "user",
+                "content":
+                    """Can you solve the following math problem? {} Explain your reasoning. Your final answer should be a single numerical number, in the form \\boxed{{answer}}, at the end of your response. """
+                    .format(question)
+            }
+        ] for agent in range(agents)
+    ]
 
-        for round in tqdm.trange(rounds, desc='rounds'):
-            for i, agent_context in enumerate(agent_contexts):
-                if round != 0:
-                    agent_contexts_other = agent_contexts[:i] + agent_contexts[i+1:]
-                    message = construct_message(agent_contexts_other, question, 2*round - 1)
-                    agent_context.append(message)
+    for round in tqdm.trange(rounds, desc='rounds'):
+      for i, agent_context in enumerate(agent_contexts):
+        if round != 0:
+          agent_contexts_other = agent_contexts[:i] + agent_contexts[i + 1:]
+          message = construct_message(
+              agent_contexts_other, question, 2 * round - 1
+          )
+          agent_context.append(message)
 
-                completion = generate_answer(agent_context)
-                assistant_message = construct_assistant_message(completion)
-                agent_context.append(assistant_message)
+        completion = generate_answer(agent_context)
+        assistant_message = construct_assistant_message(completion)
+        agent_context.append(assistant_message)
 
-        generated_description[question] = (agent_contexts, answer)
+    generated_description[question] = (agent_contexts, answer)
 
-    from tk.debate import utils
-    utils.save(cfg, generated_description)
+  from tk.debate import utils
+  utils.save(cfg, generated_description)
