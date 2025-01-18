630819f2ae65f6c01a805283371ad775213bebc5
Toni Kukurin <toni.kukurin@quantco.com>
2025-01-18 09:25:14 +0100

    add example dynamics as comment twoval

--- src/tk/rpe/tasks/custom/twoval.py
+++ src/tk/rpe/tasks/custom/twoval.py
@@ -1,4 +1,44 @@
 """Custom generalization task with n-way dependencies.
+
+Example train dynamics (default config but sequence <=16 toks, 2 it/s on T4):
+{'step': 0, 'train_loss': 3.717655897140503, 'train_accuracy': 0.013822115957736969}
+{'step': 100, 'train_loss': 2.959900140762329, 'train_accuracy': 0.0703125}
+{'step': 200, 'train_loss': 2.7089343070983887, 'train_accuracy': 0.1647135466337204}
+{'step': 300, 'train_loss': 2.5639524459838867, 'train_accuracy': 0.1931818276643753}
+{'step': 400, 'train_loss': 2.5840137004852295, 'train_accuracy': 0.16899670660495758}
+{'step': 500, 'train_loss': 2.2197928428649902, 'train_accuracy': 0.22940342128276825}
+{'step': 600, 'train_loss': 1.8815103769302368, 'train_accuracy': 0.3179687559604645}
+{'step': 700, 'train_loss': 0.7366040945053101, 'train_accuracy': 0.56640625}
+
+---
+
+{'length': 1, 'accuracy': np.float64(1.0)}
+{'length': 2, 'accuracy': np.float64(0.89453125)}
+{'length': 3, 'accuracy': np.float64(0.8274739682674408)}
+{'length': 4, 'accuracy': np.float64(0.78369140625)}
+{'length': 5, 'accuracy': np.float64(0.7632812485098839)}
+{'length': 6, 'accuracy': np.float64(0.7578125149011612)}
+{'length': 7, 'accuracy': np.float64(0.718191996216774)}
+{'length': 8, 'accuracy': np.float64(0.645263671875)}
+{'length': 9, 'accuracy': np.float64(0.582899309694767)}
+{'length': 10, 'accuracy': np.float64(0.574414074420929)}
+{'length': 11, 'accuracy': np.float64(0.547585241496563)}
+{'length': 12, 'accuracy': np.float64(0.5136718973517418)}
+{'length': 13, 'accuracy': np.float64(0.5220853611826897)}
+{'length': 14, 'accuracy': np.float64(0.459263414144516)}
+{'length': 15, 'accuracy': np.float64(0.39700523391366005)}
+{'length': 16, 'accuracy': np.float64(0.374267578125)}
+{'length': 17, 'accuracy': np.float64(0.3877527602016926)}
+{'length': 18, 'accuracy': np.float64(0.4044053815305233)}
+{'length': 19, 'accuracy': np.float64(0.3842516466975212)}
+{'length': 20, 'accuracy': np.float64(0.29521485045552254)}
+{'length': 21, 'accuracy': np.float64(0.1935453899204731)}
+{'length': 22, 'accuracy': np.float64(0.14701705053448677)}
+{'length': 23, 'accuracy': np.float64(0.13637907803058624)}
+{'length': 24, 'accuracy': np.float64(0.12744140904396772)}
+{'length': 25, 'accuracy': np.float64(0.12484374735504389)}
+{'length': 26, 'accuracy': np.float64(0.1240234412252903)}
+{'length': 27, 'accuracy': np.float64(0.12666377425193787)}
 """
 
 from tk.rpe.tasks import task
